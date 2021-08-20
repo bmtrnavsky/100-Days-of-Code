8814df3f9651710d97a0ebee2483ca035956f894
bmtrnavsky <bmtrnavsky@msn.com>
2021-08-19 21:41:58 -0500

    Update day4_love_score_calc.py
    This program automates a "True Love" quiz from a magazine. The goal was to practice count, lower, and control flow. The first version was not totaling properly. I thought that it was a problem with my algorithm, but it turns out my string concatenation was actually just totaling the True and Love and integers and not scoring correctly. After the second cleaner algorithm didn't work I started testing all my totals and found the issue. A lot of learning happened for such a simple program.

--- day4_love_score_calc.py
+++ day4_love_score_calc.py
@@ -1,25 +1,36 @@
 print("Welcome to the Love Calculator!")
+#collect data
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
-name1_lower = name1.lower()
-name2_lower = name2.lower()
+# Combine names
+tl_name = name1 + name2
 
-name1_true = name1_lower.count('t') + name1_lower.count('r') + name1_lower.count('u') + name1_lower.count('e')
+#convert to lower
 
-name1_love = name1_lower.count('l') + name1_lower.count('o') + name1_lower.count('v') + name1_lower.count('e') 
+tl_name_lower = tl_name.lower()
 
-name2_true = name2_lower.count('t') + name2_lower.count('r') + name2_lower.count('u') + name2_lower.count('e')
+# Count and add True
+t = tl_name_lower.count('t')
+r = tl_name_lower.count('r')
+u = tl_name_lower.count('u')
+e = tl_name_lower.count('e')
+true = t + r + u + e
 
-name2_love = name2_lower.count('l') + name2_lower.count('o') + name2_lower.count('v') + name2_lower.count('e') 
+# Count and add Love
+l = tl_name_lower.count('l')
+o = tl_name_lower.count('o')
+v = tl_name_lower.count('v')
+e = tl_name_lower.count('e')
+love = l + o + v + e
 
-love_score_t = name1_true + name2_true
-love_score_l = name1_love + name2_love
-
-love_score_str = str(love_score_t + love_score_l)
+#convert to string and concatinate
+love_score_str = str(true) + str(love)
+print(love_score_str)
+#Convert to int
 love_score_int = int(love_score_str)
-print(love_score_int)
 
+#output 
 if love_score_int < 10 or love_score_int > 90:
   print(f"Your score is {love_score_str}, you go together like coke and mentos.")
 elif love_score_int > 40 and love_score_int < 50:
