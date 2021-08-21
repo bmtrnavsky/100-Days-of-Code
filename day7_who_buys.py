b049e050854497d57a0ccd8b05c1e670b51a19f0
bmtrnavsky <bmtrnavsky@msn.com>
2021-08-21 09:35:49 -0500

    Create day7_who_buys.py
    In this file, I was practicing using lists and random to create a small application that randomly selects a name from a list to determine who among a group of friends would buy lunch. it is using the split method to take a user input and create a list, then len to determine a range for a random number generator to pick a position.

---
+++ day7_who_buys.py
@@ -0,0 +1,13 @@
+import random 
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+#determine length of list
+length = len(names)
+#use len to select from a range 
+position = random.randrange(0, length)
+#print name at random position
+person = names[position]
+print(f"{person}, is going to buy the meal today!")
