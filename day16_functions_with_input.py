50b9014e7b8dc716916e3d9e15060c39491962ab
Brad Trnavsky <bmtrnavsky@msn.com>
2021-09-01 22:30:18 -0500

    Create day16_functions_with_input.py
    Just a super simple practice with writing and calling a function then adding an input.

---
+++ day16_functions_with_input.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("Hello")
+  print("Functions are fun.")
+  print("I'm happy I can remember this.")
+
+greet()
+
+
+def greet_with_name(name):
+  print(f"How are you {name}?")
+
+greet_with_name("Brad")
