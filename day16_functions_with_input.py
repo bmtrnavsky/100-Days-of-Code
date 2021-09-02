55a3e73102259d5bf62f0617f93b16e2e4bb1ccb
Brad Trnavsky <bmtrnavsky@msn.com>
2021-09-01 22:50:20 -0500

    Update day16_functions_with_input.py

--- day16_functions_with_input.py
+++ day16_functions_with_input.py
@@ -3,15 +3,21 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
-def greet():
-  print("Hello")
-  print("Functions are fun.")
-  print("I'm happy I can remember this.")
+#def greet():
+  #print("Hello")
+  #print("Functions are fun.")
+  #print("I'm happy I can remember this.")
 
-greet()
+#greet()
 
 
-def greet_with_name(name):
-  print(f"How are you {name}?")
+#def greet_with_name(name):
+  #print(f"How are you {name}?")
 
-greet_with_name("Brad")
+#greet_with_name("Brad")
+
+def greet_with_name(name, location):
+ print(f"Hello {name}.")
+ print(f"What is it like in {location}?")
+
+greet_with_name(name = "Brad", location = "Longview Tx") 
