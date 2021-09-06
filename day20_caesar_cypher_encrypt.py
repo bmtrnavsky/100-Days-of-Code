9bdb470139eeee74eb22dfa61a2cd5cebe400249
Brad Trnavsky <bmtrnavsky@msn.com>
2021-09-05 22:51:59 -0500

    Create day20_caesar_cypher_encrypt.py
    This is the encrypt portion of a Caesar Cypher.  I was trying to use % 26 to fix numbers at the end but figured out I could include the alphabet 2x and still get a good answer. not elegant but functional at this stage of learning.

---
+++ day20_caesar_cypher_encrypt.py
@@ -0,0 +1,17 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, shift_ammount):
+
+  cypher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_ammount
+    new_letter = alphabet[new_position]
+    cypher_text += new_letter
+  print(f"The encoded text is: {cypher_text}")
+    
+encrypt(plain_text=text, shift_ammount=shift)
